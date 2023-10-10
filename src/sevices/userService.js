8776f9f2d6c29b2fb76ee553cd6271a9e660f4ca
dangdung595 <dangdungone@gamil.com>
2023-10-10 22:33:12 +0700

    video 52

--- src/sevices/userService.js
+++ src/sevices/userService.js
@@ -115,26 +115,28 @@ let CreateNewUser = (data) => {
       if(check === true){
         resolve({
           errCode: 1 ,
-          message: "your email is aready in used , plz another email",
+          errMessage: "your email is aready in used , plz another email",
+        });
+      }else{
+
+        let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+        await db.User.create({
+          email: data.email,
+          password: hashPasswordFromBcrypt,
+          firstName: data.firstName,
+          lastName: data.lastName,
+          address: data.address,
+          phonenumber: data.phonenumber,
+          gender: data.gender === "1" ? true : false,
+          image: data.image,
+          roleId: data.roleId,
+        });
+        resolve({
+          errCode: 0 ,
+          message: "ok",
         });
       }
 
-      let hashPasswordFromBcrypt = await hashUserPassword(data.password);
-      await db.User.create({
-        email: data.email,
-        password: hashPasswordFromBcrypt,
-        firstName: data.firstName,
-        lastName: data.lastName,
-        address: data.address,
-        phonenumber: data.phonenumber,
-        gender: data.gender === "1" ? true : false,
-        image: data.image,
-        roleId: data.roleId,
-      });
-      resolve({
-        errCode: 0 ,
-        message: "ok",
-      });
 
     }catch(e){
       reject(e)
